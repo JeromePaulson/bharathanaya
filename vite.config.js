2d5ed5dcde456909d14d1532149ca74301e5a5bc
jerome <jeromepaulson0330@gmail.com>
2026-04-26 17:11:01 +0530

    commit 5

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative paths in the built index.html
+  base: '/', // Netlify serves from the root domain, so '/' is best for SPA routing
 })
